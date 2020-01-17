1b4a30baea01bba46da6c5df9e2027c85acac0bb
mihai <myhay.360@gmail.com>
2020-01-17 18:19:26 +0100

    WIP.

--- receiver/main/app_main.c
+++ receiver/main/app_main.c
@@ -16,18 +16,7 @@
 #include "freertos/semphr.h"
 #include "freertos/queue.h"
 
-// #include "lwip/sockets.h"
-// #include "lwip/dns.h"
-// #include "lwip/netdb.h"
-// #include "lwip/igmp.h"
 
-// #include "esp_wifi.h"
-// #include "esp_system.h"
-// #include "esp_event.h"
-// #include "esp_event_loop.h"
-// #include "nvs_flash.h"
-// #include "soc/rtc_cntl_reg.h"
-// #include "rom/cache.h"
 #include "driver/spi_slave.h"
 #include "esp_log.h"
 #include "esp_spi_flash.h"
@@ -47,11 +36,15 @@ sending a transaction. As soon as the transaction is done, the line gets set low
 /*
 Pins in use. The SPI Master can use the GPIO mux, so feel free to change these if needed.
 */
-// #define GPIO_HANDSHAKE GPIO_NUM_27
-#define GPIO_MOSI GPIO_NUM_13
-#define GPIO_MISO GPIO_NUM_12
-#define GPIO_SCLK GPIO_NUM_14
-#define GPIO_CS GPIO_NUM_10
+
+// VSPI
+#define GPIO_MOSI GPIO_NUM_23
+#define GPIO_MISO GPIO_NUM_19
+#define GPIO_SCLK GPIO_NUM_18
+#define GPIO_CS GPIO_NUM_33
+#define GPIO_HANDSHAKE GPIO_NUM_21 // TODO: Investigate issue with GPIO 32 as output. (Always pull UP)
+
+
 
 // //Called after a transaction is queued and ready for pickup by master. We use this to set the handshake line high.
 // void my_post_setup_cb(spi_slave_transaction_t *trans) {
@@ -66,9 +59,22 @@ Pins in use. The SPI Master can use the GPIO mux, so feel free to change these i
 //Main application
 void app_main()
 {
-    int n=0;
     esp_err_t ret;
 
+    // Configuration for the handshake line
+    gpio_config_t io_conf={
+        .intr_type=GPIO_INTR_DISABLE,
+        .mode=GPIO_MODE_OUTPUT,
+        .pin_bit_mask=(1ULL<<GPIO_HANDSHAKE)
+    };
+
+    //Configure handshake line as output
+    gpio_config(&io_conf);
+    //Enable pull-ups on SPI lines so we don't detect rogue pulses when no master is connected.
+    gpio_set_pull_mode(GPIO_MOSI, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(GPIO_SCLK, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(GPIO_CS, GPIO_PULLUP_ONLY);
+    
     //Configuration for the SPI bus
     spi_bus_config_t buscfg={
         .mosi_io_num=GPIO_MOSI,
@@ -84,46 +90,48 @@ void app_main()
         .flags=0
     };
 
-    //Configuration for the handshake line
-    // gpio_config_t io_conf={
-    //     .intr_type=GPIO_INTR_DISABLE,
-    //     .mode=GPIO_MODE_OUTPUT,
-    //     .pin_bit_mask=(1<<GPIO_HANDSHAKE)
-    // };
-
-    //Configure handshake line as output
-    // gpio_config(&io_conf);
-    //Enable pull-ups on SPI lines so we don't detect rogue pulses when no master is connected.
-    gpio_set_pull_mode(GPIO_MOSI, GPIO_PULLUP_ONLY);
-    gpio_set_pull_mode(GPIO_SCLK, GPIO_PULLUP_ONLY);
-    gpio_set_pull_mode(GPIO_CS, GPIO_PULLUP_ONLY);
-
     //Initialize SPI slave interface
-    ret=spi_slave_initialize(HSPI_HOST, &buscfg, &slvcfg, 1);
+    ret=spi_slave_initialize(VSPI_HOST, &buscfg, &slvcfg, 1);
     assert(ret==ESP_OK);
 
-    WORD_ALIGNED_ATTR char sendbuf[129]="";
-    WORD_ALIGNED_ATTR char recvbuf[129]="";
-    memset(recvbuf, 0, 33);
+    //////////////////////////////////////////////////////////////////////
+
+    // WORD_ALIGNED_ATTR char sendbuf[129]="";
+    // WORD_ALIGNED_ATTR char recvbuf[129]="";
+    // memset(recvbuf, 0, 33);
     spi_slave_transaction_t t;
     memset(&t, 0, sizeof(t));
+    uint8_t *sendbuf =  heap_caps_malloc(256, MALLOC_CAP_DMA);
+    uint8_t *recvbuf =  heap_caps_malloc(256, MALLOC_CAP_DMA);
+    memset(sendbuf, 0, 256);
+    
 
+    int n=0;
     while(1) {
+        
         //Clear receive buffer, set send buffer to something sane
-        memset(recvbuf, 0xA5, 129);
-        sprintf(sendbuf, "This is the receiver, sending data for transmission number %04d.", n);
+        memset(recvbuf, 0, 256);
+        sprintf((char*)sendbuf, "Receiver, sending data for transmission no. %04d.", n);
+
+        if( n%5 == 0 || n%6 == 0 ){
+            printf("yeeeeean\n");
+            WRITE_PERI_REG(GPIO_OUT_W1TS_REG, (1ULL<<GPIO_HANDSHAKE));
+        } else {
+            WRITE_PERI_REG(GPIO_OUT_W1TC_REG, (1ULL<<GPIO_HANDSHAKE));
+        }
 
         //Set up a transaction of 128 bytes to send/receive
         t.length=128*8;
         t.tx_buffer=sendbuf;
         t.rx_buffer=recvbuf;
+
         /* This call enables the SPI slave interface to send/receive to the sendbuf and recvbuf. The transaction is
         initialized by the SPI master, however, so it will not actually happen until the master starts a hardware transaction
         by pulling CS low and pulsing the clock etc. In this specific example, we use the handshake line, pulled up by the
         .post_setup_cb callback that is called as soon as a transaction is ready, to let the master know it is free to transfer
         data.
         */
-        ret=spi_slave_transmit(HSPI_HOST, &t, portMAX_DELAY);
+        ret=spi_slave_transmit(VSPI_HOST, &t, portMAX_DELAY);
 
         //spi_slave_transmit does not return until the master has done a transmission, so by here we have sent our data and
         //received data from the master. Print it.
--- sender/main/app_main.c
+++ sender/main/app_main.c
@@ -13,55 +13,110 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "freertos/queue.h"
 
-#include "driver/spi_master.h"
 #include "esp_log.h"
-#include "esp_spi_flash.h"
+#include "driver/spi_master.h"
+// #include "esp_spi_flash.h"
 
 #include "soc/gpio_reg.h"
 #include "driver/gpio.h"
-#include "esp_intr_alloc.h"
+// #include "esp_intr_alloc.h"
 
 
 /*
 Pins in use. The SPI Master can use the GPIO mux, so feel free to change these if needed.
 */
-// #define GPIO_HANDSHAKE GPIO_NUM_27
+// HSPI
 #define GPIO_MOSI GPIO_NUM_13
-#define GPIO_MISO GPIO_NUM_12
+#define GPIO_MISO GPIO_NUM_5
 #define GPIO_SCLK GPIO_NUM_14
-#define GPIO_CS GPIO_NUM_10
+#define GPIO_CS GPIO_NUM_15
+#define GPIO_HANDSHAKE GPIO_NUM_32
 
+static spi_device_handle_t handle;
 
-//The semaphore indicating the slave is ready to receive stuff.
-// static xQueueHandle rdySem;
+int8_t read(spi_device_handle_t handler, uint8_t *data, uint16_t len) {
+	static struct spi_transaction_t trans;
 
-// /*
-// This ISR is called when the handshake line goes high.
-// */
-// static void IRAM_ATTR gpio_handshake_isr_handler(void* arg)
-// {
-//     //Sometimes due to interference or ringing or something, we get two irqs after eachother. This is solved by
-//     //looking at the time between interrupts and refusing any interrupt too close to another one.
-//     static uint32_t lasthandshaketime;
-//     uint32_t currtime=xthal_get_ccount();
-//     uint32_t diff=currtime-lasthandshaketime;
-//     if (diff<240000) return; //ignore everything <1ms after an earlier irq
-//     lasthandshaketime=currtime;
+	trans.flags = 0;
+	trans.length = len * 8;
+	trans.rxlength = trans.length;
+	trans.tx_buffer = heap_caps_malloc(1, MALLOC_CAP_DMA);
+	trans.rx_buffer = heap_caps_malloc(len, MALLOC_CAP_DMA);
 
-//     //Give the semaphore.
-//     BaseType_t mustYield=false;
-//     xSemaphoreGiveFromISR(rdySem, &mustYield);
-//     if (mustYield) portYIELD_FROM_ISR();
-// }
+	memset(trans.rx_buffer, 0, len*sizeof(uint8_t));
+
+	/* debug */
+	// ESP_LOG_LEVEL(ESP_LOG_DEBUG, "main", "length = %d", len);
+	// ESP_LOG_LEVEL(ESP_LOG_DEBUG, "main", "reg_addr = %x", reg_addr);
+
+	int ret = ESP_OK;
+	ret = spi_device_transmit(handler, &trans);
+	if (ret < 0) {
+		ESP_LOGE("main", "Error: transaction transmission failled");
+        return ret;
+	}
+
+	memcpy(data, trans.rx_buffer, len);
+
+    free(trans.tx_buffer);
+    free(trans.rx_buffer);
+
+	/* debug */
+	// for (int i = 0; i < len; i++)
+	// 	ESP_LOG_LEVEL(ESP_LOG_DEBUG, "main", "rx_buffer[%d] = %d %x", i, ((uint8_t *) trans.rx_buffer)[i], ((uint8_t *) trans.rx_buffer)[i]);
+
+	return ret;
+}
+
+int8_t write(spi_device_handle_t handler, uint8_t *data, uint16_t len) {
+	static struct spi_transaction_t trans;
+
+	/* debug */
+	// ESP_LOG_LEVEL(ESP_LOG_DEBUG, "main", "length = %d", len);
+	// ESP_LOG_LEVEL(ESP_LOG_DEBUG, "main", "reg_addr = %x", reg_addr);
+	
+	trans.flags = 0;
+	trans.length = len * 8;
+	trans.tx_buffer = heap_caps_malloc(len, MALLOC_CAP_DMA);
+
+	memcpy(trans.tx_buffer, data, len);
+
+	int8_t ret;
+	ret = spi_device_transmit(handler, &trans);
+	if (ret < 0) {
+		ESP_LOGE("main", "Error: transaction transmission failled");
+        return ret;
+	}
+
+    free(trans.tx_buffer);
+
+	/* debug */
+	// for (int i = 0; i < len; i++)
+	// 	ESP_LOG_LEVEL(ESP_LOG_DEBUG, "main", "tx_buffer[%d] = [%x] %d", i, ((uint8_t *) trans.tx_buffer)[i], ((uint8_t *) trans.tx_buffer)[i]);
+
+	return ret;
+}
+
 
 //Main application
 void app_main()
 {
+    // Create the semaphore.
+    // rdyToReadSem=xSemaphoreCreateBinary();
+
+    //GPIO config for the handshake line.
+    gpio_config_t io_conf={
+        .intr_type=GPIO_PIN_INTR_DISABLE,
+        .mode=GPIO_MODE_INPUT,
+        .pull_up_en=1,
+        .pin_bit_mask=(1ULL<<GPIO_HANDSHAKE)
+    };
+
+    //Set up handshake line interrupt.
+    gpio_config(&io_conf);
+ 
     esp_err_t ret;
-    spi_device_handle_t handle;
 
     //Configuration for the SPI bus
     spi_bus_config_t buscfg={
@@ -77,7 +132,7 @@ void app_main()
         .command_bits=0,
         .address_bits=0,
         .dummy_bits=0,
-        .clock_speed_hz= 9 *1000 *1000,
+        .clock_speed_hz= 1*  1000 *1000,
         .duty_cycle_pos= 128,        //50% duty cycle
         .mode=0,
         .spics_io_num=GPIO_CS,
@@ -85,56 +140,52 @@ void app_main()
         .queue_size=3
     };
 
-    //GPIO config for the handshake line.
-    // gpio_config_t io_conf={
-    //     .intr_type=GPIO_PIN_INTR_POSEDGE,
-    //     .mode=GPIO_MODE_INPUT,
-    //     .pull_up_en=1,
-    //     .pin_bit_mask=(1<<GPIO_HANDSHAKE)
-    // };
-
-    int n=0;
-    char sendbuf[128] = {0};
-    char recvbuf[128] = {0};
-    spi_transaction_t t;
-    memset(&t, 0, sizeof(t));
-
-    //Create the semaphore.
-    // rdySem=xSemaphoreCreateBinary();
-
-    //Set up handshake line interrupt.
-    // gpio_config(&io_conf);
-    // gpio_install_isr_service(0);
-    // gpio_set_intr_type(GPIO_HANDSHAKE, GPIO_PIN_INTR_POSEDGE);
-    // gpio_isr_handler_add(GPIO_HANDSHAKE, gpio_handshake_isr_handler, NULL);
-
     //Initialize the SPI bus and add the device we want to send stuff to.
     ret=spi_bus_initialize(HSPI_HOST, &buscfg, 1);
     assert(ret==ESP_OK);
     ret=spi_bus_add_device(HSPI_HOST, &devcfg, &handle);
     assert(ret==ESP_OK);
 
-    //Assume the slave is ready for the first transmission: if the slave started up before us, we will not detect 
-    //positive edge on the handshake line.
-    // xSemaphoreGive(rdySem);
+    int n=0;
+    // char sendbuf[128] = {0};
+    // char recvbuf[128] = {0};
+    // spi_transaction_t t;
+    // memset(&t, 0, sizeof(t));
+
+    // //Assume the slave is ready for the first transmission: if the slave started up before us, we will not detect 
+    // //positive edge on the handshake line.
+    // // xSemaphoreGive(rdyToReadSem);
 
     while(1) {
-        int res = snprintf(sendbuf, sizeof(sendbuf),
-                "Sender, transmission no. %04i. Last time, I received: \"%s\"", n, recvbuf);
-        if (res >= sizeof(sendbuf)) {
-            printf("Data truncated\n");
+        // int res = snprintf(sendbuf, sizeof(sendbuf),
+        //         "Sender, transmission no. %04i. Last time, I received: \"%s\"", n, recvbuf);
+        // if (res >= sizeof(sendbuf)) {
+        //     printf("Data truncated\n");
+        // }
+        // t.length=sizeof(sendbuf)*8;
+        // t.tx_buffer=sendbuf;
+        // t.rx_buffer=recvbuf;
+        // //Wait for slave to be ready for next byte before sending
+        // // xSemaphoreTake(rdyToReadSem, portMAX_DELAY); //Wait until slave is ready
+        // ret=spi_device_transmit(handle, &t);
+        // printf("Received: %s\n", recvbuf);
+        // n++;
+
+        char sendbuf[128] = {0};
+        snprintf(sendbuf, 128, "Sender, transmission no. %04i.", n);
+        if(gpio_get_level(GPIO_HANDSHAKE) == 1){
+            printf("READ\n");
+            char recvbuf[128] = {0};
+            read(handle, (uint8_t*) &recvbuf, 128);
+            printf("Received: %s\n", recvbuf);
+        } else {
+            printf("WRITE\n");
+            printf("->%s\n",sendbuf);
+            write(handle, (uint8_t*) &sendbuf, 128);
+            
         }
-        t.length=sizeof(sendbuf)*8;
-        t.tx_buffer=sendbuf;
-        t.rx_buffer=recvbuf;
-        //Wait for slave to be ready for next byte before sending
-        // xSemaphoreTake(rdySem, portMAX_DELAY); //Wait until slave is ready
-        ret=spi_device_transmit(handle, &t);
-        printf("Received: %s\n", recvbuf);
         n++;
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-
+        vTaskDelay(2500 / portTICK_PERIOD_MS);
     }
 
     //Never reached.
